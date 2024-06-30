38cc1edc0a674e78826aec22ba763a05fed689ce
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-06-30 07:27:18 +0530

    Update StickerId.py

--- plugins/Extra/StickerId.py
+++ plugins/Extra/StickerId.py
@@ -9,13 +9,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import User, Message, Sticker, Document
 
-    
-bughunter0 = Client(
-    "Sticker-Bot",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
-)
 
 START_STRING = """ Hi {}, I'm Sticker Bot. 
 
