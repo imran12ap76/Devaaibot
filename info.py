50314dc8f49dd9bfdd2393b302cc04ec4f6c5fe6
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-10-13 11:59:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,6 +51,35 @@ DATABASE_URI5="mongodb+srv://spidez5:spidez5@cluster0.h6u9g.mongodb.net/?retryWr
 DATABASE_NAME = environ.get('DATABASE_NAME', "SpidyMovie")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
+
+# Online Stream and Download
+PORT = int(environ.get('PORT', 8080))
+NO_PORT = False
+APP_NAME = None
+if 'DYNO' in environ:
+    ON_HEROKU = True
+    APP_NAME = environ.get('APP_NAME')
+else:
+    ON_HEROKU = False
+BIND_ADRESS = '0.0.0.0'
+FQDN = APP_NAME+'.herokuapp.com'
+URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "http://{}:{}/".format(FQDN, PORT)
+SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
+WORKERS = int(environ.get('WORKERS', '4'))
+SESSION_NAME = str(environ.get('SESSION_NAME', 'Spidyf2link'))
+MULTI_CLIENT = False
+name = str(environ.get('name', 'SpidyMV'))
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
+ON_HEROKU = True
+APP_NAME = ""
+HAS_SSL=True
+if HAS_SSL:
+    URL = "https://{}/".format(FQDN)
+else:
+    URL = "http://{}/".format(FQDN)
+REPO_OWNER = "Abhishek-issac"
+
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'moneykamalo.com')
