ff0423a486f7fbd3c03b1dc8f9dfd54b067f3a55
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-10-13 17:00:06 +0530

    Update custom_dl.py

--- util/custom_dl.py
+++ util/custom_dl.py
@@ -10,7 +10,7 @@ import asyncio
 import logging
 from info import *
 from typing import Dict, Union
-from lazybot import work_loads
+from zzint import work_loads
 from pyrogram import Client, utils, raw
 from .file_properties import get_file_ids
 from pyrogram.session import Session, Auth
