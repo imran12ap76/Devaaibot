b4ef2bf536c315e242c5c7c1447f1e53b7cbc25c
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-10-17 20:39:42 +0530

    Update __init__.py

--- lazybot/__init__.py
+++ lazybot/__init__.py
@@ -1,14 +1,30 @@
 import logging
 import logging.config
+import datetime
+from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
+LOGDIR = f"{__name__}/logs"
 
-# Use only one logging setup
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+# Make Logs directory if it does not exixts
+if not path.isdir(LOGDIR):
+    mkdir(LOGDIR)
+
+LOGFILE = f"{LOGDIR}/{__name__}_{LOG_DATETIME}_log.txt"
+
+file_handler = FileHandler(filename=LOGFILE)
+stdout_handler = StreamHandler(stdout)
+
+basicConfig(
+    format="%(asctime)s - [Alita_Robot] - %(levelname)s - %(message)s",
+    level=INFO,
+    handlers=[file_handler, stdout_handler],
 )
 
+getLogger("pyrogram").setLevel(WARNING)
+LOGGER = getLogger(__name__)
+
+
 # Set specific loggers' levels
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
 logging.getLogger("imdbpy").setLevel(logging.ERROR)
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
