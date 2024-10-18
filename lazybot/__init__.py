fe483c8da0aa8daf75513910c58d86ffa6058635
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-10-18 18:40:08 +0530

    Update __init__.py

--- lazybot/__init__.py
+++ lazybot/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import logging.config
+from os import path
 from datetime import datetime
 from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
 LOG_DATETIME = datetime.now().strftime("%d_%m_%Y-%H_%M_%S")
