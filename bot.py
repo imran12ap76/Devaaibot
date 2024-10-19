f47aba462001c40147c1c7ff626c7bd5f8a03c08
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-10-19 07:39:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,14 +10,14 @@ import logging.config
 # Get logging configurations
 logging.config.fileConfig('logging.conf')
 logging.getLogger().setLevel(logging.INFO)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("imdbpy").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.INFO)
+logging.getLogger("imdbpy").setLevel(logging.INFO)
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
-logging.getLogger("aiohttp").setLevel(logging.ERROR)
-logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
+logging.getLogger("aiohttp").setLevel(logging.INFO)
+logging.getLogger("aiohttp.web").setLevel(logging.INFO)
 
 
 from pyrogram import Client, __version__
