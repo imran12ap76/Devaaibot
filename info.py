c301f40629078bad0d0f4d4e31b21cf0e7d4ff77
imran12ap76 <77523586+imran12ap76@users.noreply.github.com>
2024-12-17 15:45:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,9 +43,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://ahamedjamal321:devabot321@cluster0.y6iufgt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://MERSAL:DEVA@cluster0.q5czj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Devabot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'devamersalindex')
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
